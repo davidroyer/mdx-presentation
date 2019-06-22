0c1135ba3fa7085190b85b311291dfc566b9ffc8
David Royer <droyer01@gmail.com>
2019-06-22 19:26:47 -0400

    chore: update

--- deck.js
+++ deck.js
@@ -3,10 +3,12 @@ import highlight from '@mdx-deck/themes/syntax-highlighter-prism'
 
 import { slides as one } from './slides/one.mdx'
 import { slides as two } from './slides/two.mdx'
+import { slides as three } from './slides/three.mdx'
+import { slides as four } from './slides/four.mdx'
 
 export const themes = [
     future,
     highlight
   ]
 
-export const slides = [...one, ...two]
+export const slides = [...one, ...two, ...three, ...four]
